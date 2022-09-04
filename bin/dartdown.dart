e4ae3c36a3a99bbcb3433a1781b4471291ecca3a
j32u4ukh <j32u4ukh@gmail.com>
2022-09-04 12:04:32 +0800

    init project

---
+++ bin/dartdown.dart
@@ -0,0 +1,6 @@
+import 'package:dartdown/dartdown.dart' as dartdown;
+
+void main(List<String> arguments) {
+  print("Hello world");
+  print('Hello world: ${dartdown.calculate()}!');
+}
---
+++ test/dartdown_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartdown/dartdown.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
